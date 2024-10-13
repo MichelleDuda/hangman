c28e36997c0a7c0efabbab9c17acb99d7210b602
Michelle Kelly <jttsaz91@students.codeinstitute.net>
2024-10-13 13:11:04 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print ("""\
+  _    _                                          
+ | |  | |                                         
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __   
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \  
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | | 
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_| 
+                      __/ |                       
+                     |___/                        
+""")
+
+print("Welcome to Hangman")
